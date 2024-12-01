51da7d0e32f8acf5a7e4caf3108c69f0f9572cde
HikaroRibeiro <hhakawama@gmail.com>
2024-12-01 12:54:09 -0300

    Testes implementados

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -25,10 +25,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
       return reply.status(409).send({
-        message: 'Email user has already exists.',
+        message: err.message,
       })
     }
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
