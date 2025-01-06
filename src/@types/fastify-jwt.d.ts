adb9b4df9064bd3022b202d6592426932a9e2c4c
HikaroRibeiro <hhakawama@gmail.com>
2025-01-06 19:46:22 -0300

    🐛 Correção no JWT
    Corrigido campo sub do usuario.

---
+++ src/@types/fastify-jwt.d.ts
@@ -0,0 +1,15 @@
+import '@fastify/jwt'
+
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
+    /* payload: {
+      id: number
+    } */
+    user: {
+      /* id: number
+      name: string
+      age: number */
+      sub: string
+    }
+  }
+}
